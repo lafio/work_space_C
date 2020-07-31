7374bfb6575bccaf2d4e4af9b870ab431c31143f
xiaoxiao <lichengxiao1995@126.com>
2020-07-31 22:39:52 +0800

    动态数组类模板-vector

---
+++ vector/for_atuo.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+    vector<int> v={1,2,3};
+    for(auto i=v.begin();i!=v.end();++i){
+        cout<<*i<<endl;
+    }
+    for(auto e:v){
+        cout<<e<<endl;
+    }
+}
