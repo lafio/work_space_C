80f756ab1e944495338b1d8ed51326647e184d5c
lichengxiao1995 <lichengxiao1995@126.com>
2020-08-13 16:59:45 +0800

    a+b problem2

--- acm.hdu.edu.cn/a+b_problem.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"Please enter two numbers,separated with space:"<<endl;
-    int a,b;
-    cin>>a>>b;
-    cout<<"Result is "<<a+b<<endl;
-    return 0;
-}
