5151937f7dda1a11a2efd21fba1c819da428304d
lichengxiao1995 <lichengxiao1995@126.com>
2020-08-18 09:18:24 +0800

    let the balloon rise

--- acm.hdu.edu.cn/1004_let_the_balloon_rise.cpp
+++ acm.hdu.edu.cn/1004_let_the_balloon_rise.cpp
@@ -2,33 +2,45 @@
 #include <vector>
 using namespace std;
 
-int main(){
+vector<string> popular(){
     int count,a,max_index;
     string c,n,x;
-    vector<string> color;
+    vector<string> color,result;
     vector<int> number;
-    while(cin>>count){
-        for(int i=0;i<count;i++){
-            cin>>c;
-            color.push_back(c);
-        }
-        for(int i=0;i<color.size();i++){
-            a=0;
-            for(int j=i;j<color.size();j++){
-                if(color[i]==color[j])
-                    a+=1;
+    while(1){
+        cin>>count;
+            if(count>0){
+            for(int i=0;i<count;i++){
+                cin>>c;
+                color.push_back(c);
             }
-            number.push_back(a);
-        }
-        for(int i=0;i<number.size();i++){
-            for(int j=i;j<number.size();j++){
-                if(number[i]>=number[j])
-                    max_index=i;
+            for(int i=0;i<color.size();i++){
+                a=0;
+                for(int j=i;j<color.size();j++){
+                    if(color[i]==color[j])
+                        a+=1;
+                }
+                number.push_back(a);
             }
+            for(int i=0;i<number.size();i++){
+                for(int j=i;j<number.size();j++){
+                    if(number[i]>=number[j])
+                        max_index=i;
+                }
+            }
+            //cout<<color[max_index]<<endl;
+            result.push_back(color[max_index]);
+            vector<int>().swap(number);
+            vector<string>().swap(color);
+        }else{
+            return result;
         }
-        cout<<color[max_index]<<endl;
-        vector<int>().swap(number);
-        vector<string>().swap(color);
     }
+}
+
+int main(){
+    vector<string> result=popular();
+    for(int i=0;i<result.size();i++)
+        cout<<result[i]<<endl;
     return 0;
 }
