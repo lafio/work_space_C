c38e961579c8393498fcf2ae993deca4f718824d
xiaoxiao <lichengxiao1995@126.com>
2020-08-24 00:25:16 +0800

    number sequence

--- acm.hdu.edu.cn/1005_number_sequence.cpp
+++ acm.hdu.edu.cn/1005_number_sequence.cpp
@@ -3,11 +3,28 @@
 using namespace std;
 
 int fx(int a,int b,int n){
+    int begin=0,end=1;
+    int r[60];
+    int flag=0;
+    r[0]=r[1]=r[2]=1;
     if(n==2||n==1){
         return 1;
-    }else return (a*fx(a,b,n-1)+b*fx(a,b,n-2))%7;
+    }else{
+        for(int i=3;i<=n && !flag;i++){
+            r[i]=(r[i-1]*a+r[i-2]*b)%7;
+            for(int j=2;j<i;j++){
+                if(r[j]==r[i] && r[j-1]==r[i-1]){
+                    begin=j;
+                    end=i;
+                    flag=1;
+                    break;
+                }
+            }
+        }
+        if(flag)return r[begin+(n-end)%(end-begin)];
+        else return r[n];
+    }
 }
-
 int main(){
     vector<int> result;
     int a,b,n;
