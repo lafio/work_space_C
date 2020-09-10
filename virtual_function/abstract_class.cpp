e8257475571b092aec9c2777dd0193e613e07415
xiaoxiao <lichengxiao1995@126.com>
2020-09-10 22:49:04 +0800

    abstract class

---
+++ virtual_function/abstract_class.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Base1{
+public:
+    virtual void display()const=0;//纯虚函数
+};
+
+class Base2:public Base1{
+public:
+    virtual void display()const;//覆盖基类的虚函数
+};
+void Base2::display()const{
+    cout<<"Base2::display()"<<endl;
+}
+
+class Derived:public Base2{
+public:
+    virtual void display()const;//覆盖基类的虚函数
+};
+void Derived::display()const{
+    cout<<"Derived::display()"<<endl;
+}
+void fun(Base1 *ptr){
+    ptr->display();
+}
+
+int main(){
+    Base2 base2;
+    Derived derived;
+    fun(&base2);
+    fun(&derived);
+    return 0;
+}
