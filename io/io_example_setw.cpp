a13ce028c254c065c8e1b206830aa9f362421ae0
xiaoxiao <lichengxiao1995@126.com>
2020-10-03 21:36:22 +0800

    ofstream & ostringstream

--- io/io_example_setw.cpp
+++ io/io_example_setw.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     double values[]={1.23,35.36,653.7,4358.24};
     string names[]={"Zoot","Jimmy","AI","Stan"};
-    cout<<setiosflags(ios_base::fixed);
+    cout<<setiosflags(ios_base::fixed);//设置输出精度为保留小数点后位数，而不是有效位数
     for(int i=0;i<4;i++)
         cout<<setiosflags(ios_base::left)//左对齐
         <<setw(6)<<names[i]
